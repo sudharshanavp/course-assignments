ff99fe14ab08add0cd02ebe17e30af6122e7b0ec
sudharshanavp <sudharshanavp@outlook.com>
2021-06-05 17:30:09 +0530

    Ex1 python solutions

--- Helsinki_Courses/Building_of_AI/ex1_advanced.py
+++ Helsinki_Courses/Building_of_AI/ex1_advanced.py
@@ -1,22 +1,12 @@
-import random
-
 portnames = ["PAN", "AMS", "CAS", "NYC", "HEL"]
+ 
 def permutations(route, ports):
-    if route == [0]:
-        i = random.choice(ports)
-        route.append(i)
-        if i in ports:
-            ports.remove(i)
-        permutations(route, ports)
-
-
-
     if not ports:
-
         print(' '.join([portnames[i] for i in route]))
 
-
-
-
+    else:
+        for index,item in enumerate(ports):
+            newports = ports[:index]+ports[index+1:]
+            permutations(route + [item], newports)
 
 permutations([0], list(range(1, len(portnames))))
